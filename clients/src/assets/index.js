912833454a307f241b9ce607db878d2b1aaf2ffa
Abhinav Kumar <abhinavkumar93043@example.com>
2024-11-27 17:00:14 +0530

    assets added

---
+++ clients/src/assets/index.js
@@ -0,0 +1,33 @@
+import logo from './logo.webp';
+import send from "./Send.svg";
+import shield from "./Shield.svg";
+import star from "./Star.svg";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import discount from "./Discount.svg";
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+import people01 from "./people01.png";
+import login from "./login.svg";
+import register from "./register.svg";
+import link from "./link.svg"
+
+export {
+  logo,
+  send,
+  shield,
+  star,
+  menu,
+  close,
+  discount,
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+  people01,
+  login,
+  register,
+  link,
+};
