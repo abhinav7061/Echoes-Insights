edc28725a878092971d9359dcaca22ed23aa2c49
Abhinav Kumar <abhinavkumar93043@gmail.com>
2025-04-12 21:38:26 +0530

    backend of shorts

--- api/router/blogRoutes.js
+++ api/router/blogRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getAllBlogSummaries, getBlog, createBlog, editBlog, isBlogWriter, deleteBlog } = require("../controller/blogController")
+const { getAllBlogSummaries, getBlog, createBlog, editBlog, isBlogWriter, deleteBlog, getShorts } = require("../controller/blogController")
 const { isAuthenticatedUser, isBlogAuthor } = require("../middlewares/auth")
 const multer = require('multer');
 const storage = multer.memoryStorage();
@@ -7,6 +7,7 @@ const upload = multer({ storage });
 const router = express.Router();
 
 router.route("/allBlogSummaries").get(getAllBlogSummaries);
+router.route("/shorts").get(getShorts);
 router.route("/createBlog").post(isAuthenticatedUser, upload.single('cover'), createBlog);
 router.route("/isAuthor/:blogId").post(isAuthenticatedUser, isBlogAuthor, isBlogWriter);
 router.route("/editBlog/:blogId").put(isAuthenticatedUser, isBlogAuthor, upload.single('cover'), editBlog);
