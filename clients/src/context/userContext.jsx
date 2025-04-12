5939d5d99d7a0185239b80cb6cbd71c938310582
Abhinav Kumar <abhinavkumar93043@gmail.com>
2025-04-12 21:35:44 +0530

    clear cookies on logout

--- clients/src/context/userContext.jsx
+++ clients/src/context/userContext.jsx
@@ -43,6 +43,7 @@ export const UserContext = ({ children }) => {
     const logout = () => {
         dispatch({ type: 'LOGOUT' });
         localStorage.removeItem('jwtToken');
+        document.cookie = "jwtToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     };
 
     return (
