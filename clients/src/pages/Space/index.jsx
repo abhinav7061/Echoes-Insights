5126cf4a8b79d51116c12fdc452bc0fe73adc2ff
Abhinav Kumar <abhinavkumar93043@gmail.com>
2025-06-14 16:46:41 +0530

    adding empty screen

--- clients/src/pages/Space/index.jsx
+++ clients/src/pages/Space/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import UserInfo from '../../components/UserInfo';
+import NoHistory from '../../components/EmptyState/NoHistory';
 const ReadingHistory = React.lazy(() => import('../../components/ReadingHistory'));
 const LikedBlogs = React.lazy(() => import('../LikedBlogs'));
 const SavedBlogs = React.lazy(() => import('../SavedBlogs'));
@@ -10,7 +11,7 @@ const Space = () => {
         <div className='z-10 relative'>
             <UserInfo />
             <Heading heading="history" navigateTo="/history" />
-            <ReadingHistory className="mb-0" shortFetch />
+            <ReadingHistory className="mb-0" emptyScreen={<NoHistory/>} shortFetch />
             <Heading heading="liked blogs" navigateTo="/likes" />
             <LikedBlogs className="mb-0" shortFetch />
             <Heading heading="saved blogs" navigateTo="/saved" />
